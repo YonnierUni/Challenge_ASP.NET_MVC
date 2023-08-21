cd8515bdf7ac222027c732e3ed9a04621057523c
Saul Enrique Rodriguez Suarez <uniyonnier@gmail.com>
2023-08-20 19:44:58 -0500

    First commit

---
+++ ChallengeProductsMVC/ChallengeProductsMVC/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PTCData;
+using System.Diagnostics;
+
+namespace ChallengeProductsMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            TrainingProductViewModel vm = new TrainingProductViewModel();
+
+            vm.HandleRequest();
+
+            return View(vm);
+        }
+
+        [HttpPost]
+        public ActionResult Index(TrainingProductViewModel vm)
+        {
+            vm.IsValid = ModelState.IsValid;
+            vm.HandleRequest();
+
+            if (vm.IsValid)
+            {
+                ModelState.Clear();
+            }
+
+            return View(vm);
+        }
+
+    }
+}
